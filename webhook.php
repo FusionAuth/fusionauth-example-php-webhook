d160a637328173be382d7a8dcee7a797f3664118
Dan Moore <github@mooreds.com>
2020-08-10 13:12:53 -0600

    Removed debugging log line.

--- webhook.php
+++ webhook.php
@@ -2,8 +2,6 @@
 require __DIR__. '/config.php';
 require __DIR__ . '/vendor/autoload.php';
 
-error_log("here");
-
 $headers = getallheaders();
 if (!$headers) {
   error_log("Invalid authorization header.");
