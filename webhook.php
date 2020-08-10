a8d0191997c4a00456c838210baeb313eb86a80e
Dan Moore <github@mooreds.com>
2020-08-10 10:00:18 -0600

    Removing two spaces in front of each line for formatting.

--- webhook.php
+++ webhook.php
@@ -1,33 +1,33 @@
 <?php
-  require __DIR__. '/config.php';
-  require __DIR__ . '/vendor/autoload.php';
+require __DIR__. '/config.php';
+require __DIR__ . '/vendor/autoload.php';
 
-  $input = file_get_contents('php://input');
+$input = file_get_contents('php://input');
 
-  $obj = json_decode($input);
-  if (!$obj) { 
-    error_log("Invalid JSON");
-    return;
-  }
-  
-  $type = $obj->event->type;
-  if ($type !== "user.password.breach") {
-    error_log("Sorry, we only handle breached password events.");
-    return;
-  }
-  
-  $user_id = $obj->event->user->id;
-  if (!$user_id) {
-    error_log("No user id");
-    return;
-  }
-  $client = new FusionAuth\FusionAuthClient($api_key, $fa_url);
-  $response = $client->deactivateUser($user_id);
-  if (!$response->wasSuccessful()) {
-    // uh oh
-    error_log("Response wasn't successful:");
-    error_log(var_export($response, TRUE));
-    return;
-  }
-  http_response_code(400);
+$obj = json_decode($input);
+if (!$obj) { 
+  error_log("Invalid JSON");
+  return;
+}
+
+$type = $obj->event->type;
+if ($type !== "user.password.breach") {
+  error_log("Sorry, we only handle breached password events.");
+  return;
+}
+
+$user_id = $obj->event->user->id;
+if (!$user_id) {
+  error_log("No user id");
+  return;
+}
+$client = new FusionAuth\FusionAuthClient($api_key, $fa_url);
+$response = $client->deactivateUser($user_id);
+if (!$response->wasSuccessful()) {
+  // uh oh
+  error_log("Response wasn't successful:");
+  error_log(var_export($response, TRUE));
+  return;
+}
+http_response_code(400);
 ?>
